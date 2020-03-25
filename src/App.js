645ad44c7c0b1c02971d6698ec59b37d05736791
Taki <laszlo@letmewebyou.com>
2020-03-25 17:07:12 +0100

    Removed BN logo

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          style={{ maxHeight: 100 }}
-          src="https://bitninja.io/themes/bitninja/assets/images/logo.png"
-          className="App-logo"
-          alt="logo"
-        />
         <p>Chat Test for Taki, bottom right </p>
+
         <Emojis symbol="⬇️" label="downward" />
       </header>
     </div>
